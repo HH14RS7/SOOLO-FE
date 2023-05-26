e5db1faec33cb0da64a1fbe8f7e1cd48ddd92912
devjjin <38846447+devjjin@users.noreply.github.com>
2023-05-26 16:41:11 +0900

    feat : 모임 등록,수정,삭제 기능 추가

--- src/components/party/PartyItem.jsx
+++ src/components/party/PartyItem.jsx
@@ -2,39 +2,41 @@ import { styled } from 'styled-components';
 import { dDayConvertor } from '../../shared/dDayConvertor';
 import { Link } from 'react-router-dom';
 import { PATH_URL } from '../../shared/constants';
+import moment from 'moment';
 
 const PartyItem = ({ party }) => {
   const {
     partyId,
     title,
-    content,
-    hostName,
-    // profileImage,
     // address,
     currentCount,
     totalCount,
-    processing,
+    recruitmentStatus,
     partyDate,
+    memberInfo,
   } = party;
 
   const dDay = dDayConvertor(partyDate);
+  const formatPartyDate = moment(partyDate).format('YYYY월 MM월 DD일 a HH:mm');
 
-  // console.log(partyDate);
   return (
     <PartyItemWrapper>
       <Link to={`${PATH_URL.PARTY_DETAIL}/${partyId}`}>
         <li key={partyId}>
           <p>디데이 : D-{dDay > 0 ? dDay : 0}</p>
           <p>제목 : {title}</p>
-          <p>내용 : {content}</p>
-          <p>모임장 : {hostName}</p>
-          {/* <ProfileImage src={profileImage} alt="profileImage" /> */}
           {/* <p>장소 : {address}</p> 지도 작업후 추가 */}
+          <p>모집상태 : {recruitmentStatus ? '모집중' : '모집마감'}</p>
+          {/* <p>모임장 : {hostName}</p> */}
           <p>
             모집인원 : {currentCount} / {totalCount}명
           </p>
-          <p>모집상태 : {processing ? '모집중' : '모집마감'}</p>
-          <p>모임시간 : {partyDate}</p>
+          <p>모임시간 : {formatPartyDate}</p>
+          {memberInfo?.map((member, i) => (
+            <div key={i}>
+              <ProfileImage src={member.profileImage} alt="profileImage" />
+            </div>
+          ))}
         </li>
       </Link>
     </PartyItemWrapper>
@@ -45,11 +47,12 @@ const PartyItemWrapper = styled.div`
   border: 1px solid black;
 `;
 
-// const ProfileImage = styled.img`
-//   width: 40px;
-//   height: 40px;
-//   object-fit: cover;
-//   border-radius: 50%;
-// `;
+const ProfileImage = styled.img`
+  width: 40px;
+  height: 40px;
+  object-fit: cover;
+  border-radius: 50%;
+  display: flex;
+`;
 
 export default PartyItem;
