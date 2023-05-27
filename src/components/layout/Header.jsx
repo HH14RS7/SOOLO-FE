a837fc86b303296c55dcf711fdcd6bf4623eaa00
Sejun0910 <choisejun99@gmail.com>
2023-05-27 10:25:24 +0900

    feat : partyDetailInfo Api 조회 정리 feat : 참여자가 있을 경우에만 프로필 노출 feat : 추가, 수정/삭제 조건 렌더링 기능 추가 feat : 모집 마감시 모임신청 비활성화 feat : header 임시 추가

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,5 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 const Header = () => {
-  return <>header</>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <Background>
+        <Container>
+          <FooterBtn onClick={() => navigate('/')}>메인</FooterBtn>
+          <FooterBtn onClick={() => navigate('/user/login')}>로그인</FooterBtn>
+          <FooterBtn onClick={() => navigate('/party/create')}>모임 생성</FooterBtn>
+        </Container>
+      </Background>
+    </>
+  );
 };
 
 export default Header;
+
+const Background = styled.div`
+  background: #dcdcdc;
+  width: 100%;
+  height: 100%;
+`;
+
+const Container = styled.div`
+  width: 390px;
+  height: 100%;
+  background: #1e1e1e;
+  margin: 0 auto;
+  padding-top: 20px;
+  padding-bottom: 20px;
+`;
+
+const FooterBtn = styled.button`
+  color: white;
+`;
