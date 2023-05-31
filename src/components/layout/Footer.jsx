c3f885a1f78eb535bf4f258e67724647d7fbd8cd
Jeong sun woo <gostmzmffl@gmail.com>
2023-05-31 15:26:15 +0900

    feat : Footer 링크 세팅 진행중

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { PATH_URL } from '../../shared/constants';
+import { styled } from 'styled-components';
 
 const Footer = () => {
   return (
-    <>
-      <span>모임 </span>
+    <TabBar>
+      <Link to={`${PATH_URL.MAIN}`}>
+        <span>모임 </span>
+      </Link>
       <Link to={`${PATH_URL.PARTY_LIST_MAP}`}>
         <button>위치 </button>
       </Link>
       <span>+</span>
       <span>채팅 </span>
-      <span>마이페이지 </span>
-    </>
+      <Link to={`${PATH_URL.MYPAGE}`}>
+        <span>마이페이지 </span>
+      </Link>
+    </TabBar>
   );
 };
 
 export default Footer;
+
+/* Tab Bar */
+const TabBar = styled.div`
+  position: relative;
+  width: 375px;
+  height: 83px;
+  background: rgba(249, 249, 249, 0.94);
+  box-shadow: 0px -0.5px 0px rgba(0, 0, 0, 0.3);
+  backdrop-filter: blur(10px);
+`;
