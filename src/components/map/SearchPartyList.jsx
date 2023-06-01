0e2930f38484661cd6df98465dcda41abd3cdfc0
devjjin <38846447+devjjin@users.noreply.github.com>
2023-06-01 13:10:31 +0900

    feat : 주변 모임리스트 문구 추가

--- src/components/map/SearchPartyList.jsx
+++ src/components/map/SearchPartyList.jsx
@@ -10,7 +10,15 @@ export default function SearchPartyList({ partyList }) {
         {partyList?.map(party => (
           <SearchPartyItem key={party.partyId} party={party} />
         ))}
+        {partyList?.length > 0 ? (
+          <div>마음에 드는 모임이 없으신가요?</div>
+        ) : (
+          <>
+            <div>주변에 모임이 없습니다</div>
+          </>
+        )}
       </ul>
+      <button>'강남역'에서 모임 만들기</button>
     </>
   );
 }
