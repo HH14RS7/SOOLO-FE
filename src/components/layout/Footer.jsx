6e0ca435279c7b2bba12099cd37e252788a8ea6f
Jeong sun woo <gostmzmffl@gmail.com>
2023-06-03 00:35:48 +0900

    design : footer icon 변경, footer icon,title 위치변경

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -19,36 +19,22 @@ const Footer = () => {
       <Tabs>
         <TabBarItem index={0}>
           <Link to={`${PATH_URL.MAIN}`}>
-            <Icon
-              paddingleft="4.5px"
-              paddingtop="3px"
-              active={activeTitle === 0}
-              onClick={() => handleTitleClick(0)}
-            >
-              <Partyicon
-                width="15px"
-                height="19.5px"
-                fill={activeTitle === 0 ? '#F63D68' : '#000000'}
-              />
+            <Icon active={activeTitle === 0} onClick={() => handleTitleClick(0)}>
+              <Partyicon fill={activeTitle === 0 ? '#F63D68' : '#000000'} />
             </Icon>
-            <Title active={activeTitle === 0}>모임</Title>
+            <Title marginLeft="29px" marginTop="34.87px" active={activeTitle === 0}>
+              모임
+            </Title>
           </Link>
         </TabBarItem>
         <TabBarItem index={1}>
           <Link to={`${PATH_URL.PARTY_LIST_MAP}`}>
-            <Icon
-              paddingleft="1.5px"
-              paddingtop="1.5px"
-              active={activeTitle === 1}
-              onClick={() => handleTitleClick(1)}
-            >
-              <Mapicon
-                width="21px"
-                height="21px"
-                fill={activeTitle === 1 ? '#F63D68' : '#000000'}
-              />
+            <Icon active={activeTitle === 1} onClick={() => handleTitleClick(1)}>
+              <Mapicon fill={activeTitle === 1 ? '#F63D68' : '#000000'} />
             </Icon>
-            <Title active={activeTitle === 1}>위치</Title>
+            <Title marginLeft="29px" marginTop="34.87px" active={activeTitle === 1}>
+              위치
+            </Title>
           </Link>
         </TabBarItem>
         <TabBarItem index={2}>
@@ -58,36 +44,25 @@ const Footer = () => {
         </TabBarItem>
         <TabBarItem index={3}>
           <Link to={`${PATH_URL.PARTY_CHAT}`}>
-            <Icon
-              paddingleft="1.5px"
-              paddingtop="3px"
-              active={activeTitle === 3}
-              onClick={() => handleTitleClick(3)}
-            >
-              <Chaticon
-                width="21px"
-                height="19.5px"
-                fill={activeTitle === 3 ? '#F63D68' : '#000000'}
-              />
+            <Icon active={activeTitle === 3} onClick={() => handleTitleClick(3)}>
+              <Chaticon fill={activeTitle === 3 ? '#F63D68' : '#000000'} />
             </Icon>
-            <Title active={activeTitle === 3}>채팅</Title>
+            <Title marginLeft="29px" marginTop="34.87px" active={activeTitle === 3}>
+              채팅
+            </Title>
           </Link>
         </TabBarItem>
         <TabBarItem index={4}>
           <Link to={`${PATH_URL.MYPAGE}`}>
-            <Icon
-              paddingleft="4.5px"
-              paddingtop="1.5px"
-              active={activeTitle === 4}
-              onClick={() => handleTitleClick(4)}
-            >
-              <Mypageicon
-                width="15px"
-                height="21px"
-                fill={activeTitle === 4 ? '#F63D68' : '#000000'}
-              />
+            <Icon active={activeTitle === 4} onClick={() => handleTitleClick(4)}>
+              <Mypageicon fill={activeTitle === 4 ? '#F63D68' : '#000000'} />
             </Icon>
-            <Title active={activeTitle === 4} left="calc(50% - 43px/2 + 0.5px)">
+            <Title
+              marginLeft="17px"
+              marginTop="34.87px"
+              active={activeTitle === 4}
+              left="calc(50% - 43px/2 + 0.5px)"
+            >
               마이페이지
             </Title>
           </Link>
@@ -121,23 +96,14 @@ const Tabs = styled.div`
 
 /* Tab Bar Item */
 const TabBarItem = styled.div`
-  display: flex;
-  text-align: center;
-  align-items: center;
-  justify-content: center;
   width: 76px;
   height: 49px;
 `;
 
 /* icon */
 const Icon = styled.div`
-  padding-left: ${props => props.paddingleft};
-  padding-top: ${props => props.paddingtop};
-  /* width: 24px; */
-  /* height: 24px; */
-  /* left: 26px; */
-  /* top: 7px; */
-
+  margin-left: 26px;
+  margin-top: 7px;
   cursor: pointer;
   transition: color 0.3s ease-in-out;
   color: ${props => (props.active ? '#F63D68' : '#000000')};
@@ -149,21 +115,11 @@ const Icon = styled.div`
 
 /* span */
 const Title = styled.span`
-  /* width: auto; */
-  /* height: 10px; */
-  /* left: ${props => props.left || 'calc(50% - 18px / 2)'};
-  top: calc(50% - 10px / 2 + 15.37px); */
-
+  margin-left: ${props => props.marginLeft};
+  margin-top: ${props => props.marginTop};
   font-family: 'Pretendard';
   font-style: normal;
   font-weight: 400;
   font-size: 10px;
-  /* line-height: 100%; */
-  /* identical to box height, or 10px */
-
-  /* text-align: center; */
-  /* letter-spacing: -0.015em; */
-
-  /* Gray/500 */
   color: ${props => (props.active ? '#F63D68' : '#667085')};
 `;
