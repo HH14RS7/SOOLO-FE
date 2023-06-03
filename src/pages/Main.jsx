cf5082107a5da09123335ba773d39f6ca5285612
devjjin <38846447+devjjin@users.noreply.github.com>
2023-06-03 21:58:17 +0900

    style : 메인의 모임만들기 버튼 삭제

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,8 +1,6 @@
 import PartyList from '../components/party/PartyList';
 import MyPartyList from '../components/party/MyPartyList';
 
-import { Link, useNavigate } from 'react-router-dom';
-import { PATH_URL } from '../shared/constants';
 import { styled } from 'styled-components';
 import Cookies from 'js-cookie';
 import { useEffect, useState } from 'react';
@@ -10,7 +8,6 @@ import { useEffect, useState } from 'react';
 const Main = () => {
   const [isLogin, setIsLogin] = useState(false);
   const token = Cookies.get('Access_key');
-  const navigate = useNavigate();
 
   useEffect(() => {
     if (token) {
@@ -18,21 +15,11 @@ const Main = () => {
     }
   }, [token]);
 
-  const handleLinkClick = e => {
-    if (!isLogin) {
-      e.preventDefault();
-      alert('로그인이 필요합니다.');
-      navigate(PATH_URL.LOGIN);
-    }
-  };
   return (
     <Background>
       <Container>
         {isLogin && <MyPartyList />}
         <PartyList />
-        <Link to={`${PATH_URL.PARTY_CREATE}`} onClick={handleLinkClick}>
-          <CreateButton>모임 만들기</CreateButton>
-        </Link>
       </Container>
     </Background>
   );
@@ -47,19 +34,6 @@ const Container = styled.div`
   width: 390px;
   height: 100%;
   margin: 0 auto;
-`;
-
-const CreateButton = styled.button`
-  position: fixed;
-  bottom: 80px;
-  right: 250px;
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
-  background: #f63d68;
-  color: white;
-  border: none;
-  outline: none;
 `;
 
 export default Main;
