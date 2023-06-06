17fd2acd515b45c1a4e086a80a884f5c37652fa1
Sejun0910 <choisejun99@gmail.com>
2023-06-06 19:12:02 +0900

    fix : .env LOGIN_URL 수정

--- src/components/user/NaverLoginComponent.jsx
+++ src/components/user/NaverLoginComponent.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NaverLoginComponent = () => {
   const NAVER_CLIENT_ID = `${process.env.REACT_APP_NAVER_CLIENT_ID}`;
-  const REDIRECT_URI = 'https://soolo-fe.vercel.app/naver/callback'; // 프론트 서버 배포 주소
+  const REDIRECT_URI = `${process.env.REACT_APP_LOGIN_REDIRECT_URI}`;
   const STATE = 'flase';
   const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=${STATE}&redirect_uri=${REDIRECT_URI}`;
 
