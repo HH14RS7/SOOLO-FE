3503867fa29cdb8a468c33970094c3b1da4308e1
devjjin <38846447+devjjin@users.noreply.github.com>
2023-06-17 19:19:51 +0900

    fix : footer분기 이외 default값 주석처리

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -37,7 +37,7 @@ const Footer = () => {
         setFooterActive(4);
         break;
       default:
-        setFooterActive(0);
+      // setFooterActive(0);
     }
   }, [location.pathname, setFooterActive]);
 
