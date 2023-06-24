6d0100b580ceabfc50a2a2b1fffd09aa675e19de
Jeong sun woo <gostmzmffl@gmail.com>
2023-06-24 21:57:12 +0900

    feat : 엑세스토큰 만료시 리프레시 토큰으로 토큰 재발급 기능 추가(카카오)

--- src/components/user/KakaoRedirection.jsx
+++ src/components/user/KakaoRedirection.jsx
@@ -3,47 +3,82 @@ import Cookies from 'js-cookie';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Loading from '../Loading';
+import crypto from 'crypto-js';
+import { postAPI } from '../../api/api';
+
+const JWT_EXPIRY_TIME = 3 * 60 * 60 * 1000;
 
 const KakaoRedirection = () => {
   const code = window.location.search;
   const navigate = useNavigate();
 
   useEffect(() => {
-    axios.post(`${process.env.REACT_APP_REDIRECT_URI}/kakao/callback${code}`).then(r => {
-      console.log('data => ', r.data);
-      console.log('header => ', r.headers);
+    axios
+      .post(`${process.env.REACT_APP_REDIRECT_URI}/kakao/callback${code}`)
+      .then(onLoginSuccess)
+      .catch(error => {
+        console.log(error);
+      });
+  }, [code]);
 
-      // data을 받아서 localStorage에 저장.
-      localStorage.setItem('memberId', r.data.data.memberId);
-      localStorage.setItem('memberUniqueId', r.data.data.memberUniqueId);
-      localStorage.setItem('memberName', r.data.data.memberName);
-      localStorage.setItem('profileImage', r.data.data.profileImage);
-      localStorage.setItem('sse', null);
+  const onSilentRefresh = () => {
+    postAPI('/reissue')
+      .then(onLoginRefresh)
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
-      // access_key설정
-      const accessKey = r.headers.get('access_key').split(' ')[1];
+  const onLoginSuccess = response => {
+    // data을 받아서 localStorage에 저장.
+    localStorage.setItem('memberId', response.data.data.memberId);
+    localStorage.setItem('memberUniqueId', response.data.data.memberUniqueId);
+    localStorage.setItem('memberName', response.data.data.memberName);
+    localStorage.setItem('profileImage', response.data.data.profileImage);
+    localStorage.setItem('sse', null);
 
-      // refresh_key설정
-      const refreshkey = r.headers.get('refresh_key').split(' ')[1];
+    // access_key설정
+    const accessKey = response.headers.get('access_key').split(' ')[1];
 
-      Cookies.set('Access_key', accessKey);
-      Cookies.set('Refresh_key', refreshkey);
+    // refresh_key설정
+    const refreshkey = response.headers.get('refresh_key').split(' ')[1];
 
-      //  Access Token 만료 시간 지정
-      // const expirationTime = new Date();
-      // expirationTime.setMinutes(expirationTime.getMinutes() + 1);
+    Cookies.set('Access_key', accessKey);
+    Cookies.set('Refresh_key', refreshkey);
 
-      //  Refresh Token 만료 시간 지정
-      // const refreshTokenExpirationTime = new Date();
-      // refreshTokenExpirationTime.setDate(refreshTokenExpirationTime.getDate() + 7);
+    // access_key설정
+    // const accessKey = response.headers.get('access_key').split(' ')[1];
+    // axios.defaults.headers.common['Access_key'] = `Bearer ${accessKey}`;
 
-      // Cookies.set('Access_key', accessKey, { expires: expirationTime });
-      // Cookies.set('Refresh_key', refreshkey, { expires: refreshTokenExpirationTime });
+    // refresh_key설정
+    // const refreshkey = response.headers.get('refresh_key').split(' ')[1];
+    // Cookies.set('Refresh_key', refreshkey);
 
-      // alert(r.data.msg);
-      navigate('/');
-    });
-  }, [code, navigate]);
+    // accessToken 만료하기 1분 전에 로그인 연장
+    setTimeout(onSilentRefresh, JWT_EXPIRY_TIME - 60000);
+    navigate('/home');
+  };
+
+  const onLoginRefresh = response => {
+    // access_key설정
+    const accessKey = response.headers.get('access_key').split(' ')[1];
+
+    // refresh_key설정
+    const refreshkey = response.headers.get('refresh_key').split(' ')[1];
+
+    Cookies.set('Access_key', accessKey);
+    Cookies.set('Refresh_key', refreshkey);
+    // access_key설정
+    // const accessKey = response.headers.get('access_key').split(' ')[1];
+    // axios.defaults.headers.common['Access_key'] = `Bearer ${accessKey}`;
+
+    // refresh_key설정
+    // const refreshkey = response.headers.get('refresh_key').split(' ')[1];
+    // Cookies.set('Refresh_key', refreshkey);
+
+    // accessToken 만료하기 1분 전에 로그인 연장
+    setTimeout(onSilentRefresh, JWT_EXPIRY_TIME - 60000);
+  };
 
   return <Loading />;
 };
